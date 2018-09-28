313948d3fce405ffb1a56114f39e8af728d6dd45
Hyungsuk Yoon <youhanmir@gmail.com>
2018-09-28 19:17:12 +0900

    brand new data download script

--- data/data_download.py
+++ data/data_download.py
@@ -1,91 +1,109 @@
-import os
-import urllib.request
-from PIL import Image
-from threading import Thread
+from pathlib import Path
 from queue import Queue
+from threading import Thread
+
+import requests
+from PIL import Image
 
 
 q = Queue(1000)
-data_folder = 'images_data'
+DATA_DIR = Path("./images_data")
+CROP_DIR = Path("./images_data_crop")
 
 
 def download(imgname, url):
-    pstr = 'download image %s ...' % imgname
+    pstr = f"download image {imgname} ..."
 
-    if url == 'None':
-        print(pstr + ' None')
+    if url == "None":
+        print(f"{pstr} None")
         return
 
-    imgpath = os.path.join(data_folder, imgname)
-    if os.path.exists(imgpath):
-        print(pstr + ' Exist, Skip!')
+    image_fullpath = DATA_DIR / imgname
+    if image_fullpath.exists():
+        print(f"{pstr} Exist, Skip!")
         return
 
-    urllib.request.urlretrieve(url, imgpath)
-    print(pstr + ' Done!')
+    response = requests.get(url, stream=True)
+    with image_fullpath.open("wb") as f:
+        for chunk in response.iter_content(chunk_size=512):
+            f.write(chunk)
+
+    print(f"{pstr} Done!")
 
 
-def crop(imgname, crop_xy):
-    pstr = 'crop image %s ...' % imgname
+def crop(image_name, crop_xy):
+    pstr = f"crop image {image_name} ..."
     crop_xy = list(map(int, crop_xy.split()))
 
-    imgpath = os.path.join(data_folder, imgname)
-    if os.path.exists(imgpath):
-        img = Image.open(imgpath)
-        area = (crop_xy[2], crop_xy[0], crop_xy[3], crop_xy[1])
-        img = img.crop(area)
-        img = img.resize((600, 800))
-        img.save(os.path.join(data_folder + '_crop', imgname))
-        print(pstr + ' Done!')
+    image_fullpath = DATA_DIR / image_name
+    if image_fullpath.exists():
+        cropped_image_fullpath = CROP_DIR / image_name
+
+        with Image.open(image_fullpath) as img:
+            crop_area = (crop_xy[2], crop_xy[0], crop_xy[3], crop_xy[1])
+            img = img.crop(crop_area)
+            img = img.resize((600, 800))
+            img.save(cropped_image_fullpath)
+        print(f"{pstr} Done!")
     else:
-        print(pstr + ' Does not exist!')
+        print(f"{pstr} Does not exist!")
 
 
 def thread_work():
     while True:
         job, params = q.get()
-        if job == 'download':
+
+        if job == "download":
             download(*params)
-        elif job == 'crop':
+        elif job == "crop":
             crop(*params)
+
         q.task_done()
 
 
 def main():
-    if not os.path.isdir(data_folder):
-        os.mkdir(data_folder)
+    if not DATA_DIR.is_dir():
+        DATA_DIR.mkdir()
 
-    for i in range(50):
+    for _ in range(50):
         t = Thread(target=thread_work)
         t.daemon = True
         t.start()
 
-    imgs_names = open('alldata_urls.txt', 'r').read().strip().split('\n')
-    for item in imgs_names:
-        q.put(['download', item.split()])
-    q.join()
-    print('\t\tDownload - Done!')
+    with open("alldata_urls.txt", "r") as f:
+        image_infos = f.read().strip().split("\n")
 
-    images = os.listdir(data_folder)
-    for item in imgs_names:
-        img_path = os.path.join(data_folder, item.split()[0])
-        if os.path.exists(img_path):
-            filesize = os.stat(img_path).st_size
-            if filesize <= 10 * 1024:
-                print('Remove %s which size %d bytes below 10 * 1024 bytes' % (
-                    item.split()[0], filesize))
-                os.remove(img_path)
+    for image_info in image_infos:
+        q.put(["download", image_info.split()])
+    q.join()
+
+    print("\t\tDownload - Done!")
+
+    for image_info in image_infos:
+        image_name = image_info.split()[0]
+
+        image_fullpath = DATA_DIR / image_name
+        if not image_fullpath.exists():
+            continue
+
+        filesize = image_fullpath.stat().st_size
+        if filesize <= 10 * 1024:
+            print(f"Remove {image_name} which size {filesize} bytes below 10 * 1024 bytes")
+            image_fullpath.unlink()
 
     # crop the downloaded images
-    if not os.path.isdir(data_folder + '_crop'):
-        os.mkdir(data_folder + '_crop')
+    if not CROP_DIR.is_dir():
+        CROP_DIR.mkdir()
 
-    crop_params = open('crop.txt', 'r').read().strip().split('\n')
-    for item in crop_params:
-        q.put(['crop', item.split(maxsplit=1)])
+    with open("crop.txt", "r") as f:
+        crop_infos = f.read().strip().split("\n")
+
+    for crop_info in crop_infos:
+        q.put(["crop", crop_info.split(maxsplit=1)])
     q.join()
-    print('\t\tEverything - Done!')
+
+    print("\t\tEverything - Done!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
